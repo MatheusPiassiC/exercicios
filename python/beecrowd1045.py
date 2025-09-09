720e39a0032e104ff4d6c96745527b65d940b876
piassi <matheuspiassi4@gmail.com>
2025-09-09 00:11:03 -0300

    beecrowd1045.py completo

--- python/beecrowd1045.py
+++ python/beecrowd1045.py
@@ -1,9 +1,27 @@
 
-def tipo_triangulo(entrada: list):
+def tipo_triangulo(list):
 
-    for i in range(len(list)):
-        if list[i] >=
+    
+    if list[2] >= (list[1] + list[0]):
+        print("NAO FORMA TRIANGULO")
+        return "erro"
+
+    if list[2] == (list[1]**2 + list[0]**2)**0.5:
+        print("TRIANGULO RETANGULO")
+    elif list[2] > (list[1]**2 + list[0]**2)**0.5:
+        print("TRIANGULO OBTUSANGULO")
+    elif list[2] < (list[1]**2 + list[0]**2)**0.5:
+        print("TRIANGULO ACUTANGULO")
+
+    if list[2] == list[1] and list[1] == list[0]:
+        print("TRIANGULO EQUILATERO")    
+    elif list[2] == list[1] or list[1] == list[0] or list[0] == list[2]:
+        print("TRIANGULO ISOSCELES")
+
+    return "tudo certo!"
+
 
 
 entrada = list(map(float, input().split()))
-print(entrada)
+entrada.sort()
+tipo_triangulo(entrada)
